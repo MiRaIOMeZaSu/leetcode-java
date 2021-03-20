ea93b784aef889add9edadafd3917ffffac15be0
miraiomezasu <3792927266@qq.com>
2021-03-20 22:42:14 +0800

    P2676:done

---
+++ src/leetcode/editor/_P2676/Main.java
@@ -0,0 +1,35 @@
+package leetcode.editor._P2676;
+
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        Scanner cin = new Scanner(System.in);
+        int size = cin.nextInt(), target = cin.nextInt();
+        int sum = 0;
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            int input = cin.nextInt();
+            if (sum > target) {
+                if (input > queue.peek()) {
+                    sum += input;
+                    queue.add(input);
+                    count++;
+                    while (sum - queue.peek() > target) {
+                        sum -= queue.peek();
+                        queue.poll();
+                        count--;
+                    }
+                }
+                continue;
+            }
+            queue.add(input);
+            sum += input;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
