d22119940c1533c80e89fef1ab5069e18ff1181a
miraiomezasu <3792927266@qq.com>
2021-03-27 23:28:25 +0800

    P4017: still MLE with all short - but not matrix

--- src/leetcode/editor/P4017/Main.java
+++ src/leetcode/editor/P4017/Main.java
@@ -9,48 +9,48 @@ public class Main {
         Scanner cin = new Scanner(System.in);
         int V = cin.nextInt();
         int E = cin.nextInt();
-        boolean graph[][] = new boolean[V + 1][V + 1];
-        int in[] = new int[V + 1];
-        int out[] = new int[V + 1];
+        List<Short>[] graph = new ArrayList[V + 1];
+        short[] in = new short[V + 1];
+        short[] out = new short[V + 1];
+        for (int i = 1; i < V + 1; i++) {
+            graph[i] = new ArrayList<>();
+        }
         for (int i = 0; i < E; i++) {
-            int A = cin.nextInt();
-            int B = cin.nextInt();
-            graph[B][A] = true;
+            short A = cin.nextShort();
+            short B = cin.nextShort();
+            graph[B].add(A);
             in[B]++;
             out[A]++;
         }
         // 使用拓扑排序
-        List<Short> queue = new ArrayList<>();
         Queue<Short> zeroIn = new LinkedList<>();
-        List<Short> ends = new ArrayList<>();
         List<Short> heads = new ArrayList<>();
         for (short i = 1; i < V + 1; i++) {
             if (in[i] == 0) {
                 zeroIn.add(i);
                 heads.add(i);
             }
-            if (out[i] == 0) {
-                ends.add(i);
-            }
         }
-        int dp[] = new int[V + 1];
-        for (int i : heads) {
+        int[] dp = new int[V + 1];
+        for (short i : heads) {
             dp[i] = 1;
         }
-        int visit[] = new int[V + 1];
+        short[] visit = new short[V + 1];
+        int count = 0;
         while (!zeroIn.isEmpty()) {
             short pivot = zeroIn.poll();
-            queue.add(pivot);
             if (dp[pivot] == 0) {
-                for (int i = 1; i < V + 1; i++) {
-                    if (graph[pivot][i]) {
-                        dp[pivot] += dp[i];
-                        dp[pivot] %= 80112002;
-                    }
+                for (int n : graph[pivot]) {
+                    dp[pivot] += dp[n];
+                    dp[pivot] %= 80112002;
+                }
+                if (out[pivot] == 0) {
+                    count += dp[pivot];
+                    count %= 80112002;
                 }
             }
             for (short i = 1; i < V + 1; i++) {
-                if (graph[i][pivot]) {
+                if (graph[i].contains(pivot)) {
                     in[i]--;
                     if (in[i] == 0 && visit[i] == 0) {
                         visit[i] = 1;
@@ -59,11 +59,6 @@ public class Main {
                 }
             }
         }
-        int count = 0;
-        for (int i : ends) {
-            count += dp[i];
-            count %= 80112002;
-        }
-        System.out.printf(String.valueOf(count));
+        System.out.printf(String.valueOf(count % 80112002));
     }
 }
