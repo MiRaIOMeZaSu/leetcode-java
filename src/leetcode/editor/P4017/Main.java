b4c3e2c25ff8d58f0c6a9ca5581db6c153231b37
miraiomezasu <3792927266@qq.com>
2021-03-27 22:54:54 +0800

    P4017: still MLE with all short

--- src/leetcode/editor/P4017/Main.java
+++ src/leetcode/editor/P4017/Main.java
@@ -5,28 +5,26 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         // 使用回溯法?拓扑排序?
+        // 使用邻接矩阵
         Scanner cin = new Scanner(System.in);
         int V = cin.nextInt();
         int E = cin.nextInt();
-        ArrayList<Integer> graph[] = new ArrayList[V + 1];
+        boolean graph[][] = new boolean[V + 1][V + 1];
         int in[] = new int[V + 1];
         int out[] = new int[V + 1];
-        for (int i = 1; i < V + 1; i++) {
-            graph[i] = new ArrayList<>();
-        }
         for (int i = 0; i < E; i++) {
             int A = cin.nextInt();
             int B = cin.nextInt();
-            graph[B].add(A);
+            graph[B][A] = true;
             in[B]++;
             out[A]++;
         }
         // 使用拓扑排序
-        List<Integer> queue = new ArrayList<>();
-        Queue<Integer> zeroIn = new LinkedList<>();
-        List<Integer> ends = new ArrayList<>();
-        List<Integer> heads = new ArrayList<>();
-        for (int i = 1; i < V + 1; i++) {
+        List<Short> queue = new ArrayList<>();
+        Queue<Short> zeroIn = new LinkedList<>();
+        List<Short> ends = new ArrayList<>();
+        List<Short> heads = new ArrayList<>();
+        for (short i = 1; i < V + 1; i++) {
             if (in[i] == 0) {
                 zeroIn.add(i);
                 heads.add(i);
@@ -41,16 +39,18 @@ public class Main {
         }
         int visit[] = new int[V + 1];
         while (!zeroIn.isEmpty()) {
-            int pivot = zeroIn.poll();
+            short pivot = zeroIn.poll();
             queue.add(pivot);
             if (dp[pivot] == 0) {
-                for (int n : graph[pivot]) {
-                    dp[pivot] += dp[n];
-                    dp[pivot] %= 80112002;
+                for (int i = 1; i < V + 1; i++) {
+                    if (graph[pivot][i]) {
+                        dp[pivot] += dp[i];
+                        dp[pivot] %= 80112002;
+                    }
                 }
             }
-            for (int i = 1; i < V + 1; i++) {
-                if (graph[i].indexOf(pivot) != -1) {
+            for (short i = 1; i < V + 1; i++) {
+                if (graph[i][pivot]) {
                     in[i]--;
                     if (in[i] == 0 && visit[i] == 0) {
                         visit[i] = 1;
