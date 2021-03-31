b5c6f062eedae87912e59a8777cd3b13d0a00698
miraiomezasu <3792927266@qq.com>
2021-03-31 10:48:46 +0800

    29: done

--- src/leetcode/editor/_29_divide/Main.java
+++ src/leetcode/editor/_29_divide/Main.java
@@ -3,6 +3,6 @@ package leetcode.editor._29_divide;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.divide(-2147483648, 2);
+        solution.divide(2147483647,3);
     }
 }
--- src/leetcode/editor/_29_divide/Solution.java
+++ src/leetcode/editor/_29_divide/Solution.java
@@ -9,43 +9,37 @@ class Solution {
             return dividend == Integer.MIN_VALUE ? 1 : 0;
         }
         int flag = (dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0) ? 1 : -1;
-        int _dividend = Math.abs(dividend);
-        int _divisor = Math.abs(divisor);
+        int _dividend = dividend > 0 ? dividend : -dividend;
+        int _divisor = divisor > 0 ? divisor : -divisor;
         if (_divisor == 1) {
             if (dividend == Integer.MIN_VALUE && divisor == -1) {
                 return Integer.MAX_VALUE;
             }
-            int ret = flag < 0 ? -_dividend : _dividend;
+            return flag < 0 ? -_dividend : _dividend;
         }
-        int time = 1;
+
+        // 循环判断开始
+        int count = 0;
         int curr = _divisor;
-        if (_dividend - curr > _divisor) {
-            _dividend -= curr;
-        } else if (_dividend - curr >= 0) {
-            return flag < 0 ? -1 : 1;
-        } else {
-            return 0;
+        while (!(_dividend < _divisor && _dividend >= 0)) {
+            if (_dividend - _divisor > _divisor) {
+                _dividend -= _divisor;
+            } else if (_dividend - _divisor >= 0) {
+                return flag < 0 ? -(count + 1) : 1 + count;
+            } else {
+                return count;
+            }
+            int d = 1;
+            count += 1;
+            while (_dividend - curr > curr) {
+                curr += curr;
+                _dividend -= curr;
+                d += d;
+                count += d;
+            }
+            // 此时_dividend必定大于零
+            curr = _divisor;
         }
-        while (_dividend - curr > _divisor) {
-            curr *= 2;
-            _dividend -= curr;
-            time++;
-        }
-        int temp = 0;
-        int d = (int) Math.pow(2, time - 1);
-        for (int i = time; i > 0; i--) {
-            temp += d;
-            d /= 2;
-        }
-        time = temp;
-        if (_dividend - _divisor < 0 && _dividend > 0) {
-            return flag < 0 ? -time : time;
-        }
-        // 考虑二分法
-        while (!(_dividend - curr < _divisor && _dividend - curr >= 0)) {
-            curr -= _divisor;
-        }
-        time += curr / _divisor;
-        return flag < 0 ? -time : time;
+        return flag < 0 ? -count : count;
     }
 }
