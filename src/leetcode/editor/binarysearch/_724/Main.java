f45d0eda0fa21a77dfb8b1aa2efb2915ee25d8e6
miraiomezasu <3792927266@qq.com>
2021-04-02 14:16:34 +0800

    b724: wrong understand

---
+++ src/leetcode/editor/binarysearch/_724/Main.java
@@ -0,0 +1,8 @@
+package leetcode.editor.binarysearch._724;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.solve(5, 4);
+    }
+}
---
+++ src/leetcode/editor/binarysearch/_724/Solution.java
@@ -0,0 +1,31 @@
+package leetcode.editor.binarysearch._724;
+
+class Solution {
+    public int solve(int n, int k) {
+        // 在n次游戏中至多赢k次的所有可能性
+        // 动态规划?
+        // 只需要计算一半的数量
+//        n ≤ 10,000,因此不能排列或暴力获取
+//        k ≤ 10
+        int ret = 0;
+        int[][] dp = new int[n + 1][k + 1];
+        for (int i = 1; i <= k; i++) {
+            dp[i][i] = 1;
+            dp[i][0] = 1;
+            dp[i][1] = i;
+            dp[i][i - 1] = i;
+        }
+        for (int i = 1; i <= n; i++) {
+            dp[i][1] = i;
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= k; i++) {
+            for (int j = 2; j <= n; j++) {
+                dp[j][i] = dp[j - 1][i] + dp[j - 1][i - 1];
+            }
+        }
+
+
+        return ret;
+    }
+}
