e03f6b7cd5b91d003721c0828d1f60dabde2f1bb
miraiomezasu <3792927266@qq.com>
2021-04-11 14:24:44 +0800

    96:fast way

--- src/leetcode/editor/_96_numTrees/Solution.java
+++ src/leetcode/editor/_96_numTrees/Solution.java
@@ -1,37 +1,23 @@
 package leetcode.editor._96_numTrees;
 
 class Solution {
-    int dp[][];
+    int dp[];
 
     public int numTrees(int n) {
         // 动态规划
-        // dp[i][j] 表示i到j范围内所有可能的二叉搜索树的数量
-        this.dp = new int[n + 1][n + 1];
-        dp[1][1] = 1;
-        // 空的一样是零
-        dp[1][0] = 1;
-        for (int i = 1; i < n; i++) {
-            dp[i + 1][i + 1] = 1;
-            dp[i][i + 1] = 2;
-            dp[i + 1][i] = 1;
+        // 直接计算长度即可
+        if (n <= 2) {
+            return n;
         }
+        dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
         for (int i = 3; i <= n; i++) {
-            // 每次的区间长度为i
-            int index = 1;
-            while (index + i - 1 <= n) {
-                dp[index][index + i - 1] = compute(index, index + i - 1);
-                index++;
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
             }
         }
-        return dp[1][n];
-    }
-
-    public int compute(int left, int right) {
-        int ret = 0;
-        for (int i = left; i <= right; i++) {
-            // 以i为根结点
-            ret += dp[left][i - 1] * dp[i + 1][right];
-        }
-        return ret;
+        return dp[n];
     }
 }
