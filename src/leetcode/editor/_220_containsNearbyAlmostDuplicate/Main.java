70544dc843aabc8bbac8367143b721f02c081713
miraiomezasu <3792927266@qq.com>
2021-06-05 18:20:17 +0800

    220:slow way

--- src/leetcode/editor/_220_containsNearbyAlmostDuplicate/Main.java
+++ src/leetcode/editor/_220_containsNearbyAlmostDuplicate/Main.java
@@ -3,8 +3,8 @@ package leetcode.editor._220_containsNearbyAlmostDuplicate;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] arr = new int[]{1,5,9,1,5,9};
-        boolean ret = solution.containsNearbyAlmostDuplicate(arr, 2, 3);
+        int[] arr = new int[]{2147483646,2147483647};
+        boolean ret = solution.containsNearbyAlmostDuplicate(arr, 3, 3);
         System.out.println(ret);
     }
 }
--- src/leetcode/editor/_220_containsNearbyAlmostDuplicate/Solution.java
+++ src/leetcode/editor/_220_containsNearbyAlmostDuplicate/Solution.java
@@ -1,15 +1,66 @@
 package leetcode.editor._220_containsNearbyAlmostDuplicate;
 
 
+import java.util.*;
+
 class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        // 维护小顶堆(以绝对值做比较)
+        // 维护一个大顶堆和一个小顶堆
         // 只变换k
-        int size = nums.length;
-        for (int i = 1; i <= k; i++) {
-            for (int j = 0; j + i < size; j++) {
-                if (Math.abs(nums[j] - nums[j + i]) <= t) {
-                    return true;
+        if (k == 0 || nums.length <= 1) {
+            return false;
+        }
+        TreeMap<Integer, Integer> minTreeMap = new TreeMap<>();
+//        TreeMap<Integer, Integer> maxTreeMap = new TreeMap<>((o1, o2) -> o2 - o1);
+        List<TreeMap<Integer, Integer>> maps = new ArrayList<>();
+        maps.add(minTreeMap);
+//        maps.add(maxTreeMap);
+        for (int i = 1; i <= k && i < nums.length; i++) {
+            int num = nums[i];
+            minTreeMap.put(num, minTreeMap.getOrDefault(num, 0) + 1);
+//            maxTreeMap.put(num, maxTreeMap.getOrDefault(num, 0) + 1);
+        }
+        for (int i = 0; i < nums.length - 1; i++) {
+            // 如何避免溢出?
+            int maxBorder = nums[i] + t;
+            int minBorder = nums[i] - t;
+            if (nums[i] > Integer.MAX_VALUE - t) {
+                // 正数溢出(此时必然不会有负溢出)
+                return minTreeMap.higherKey(minBorder - 1) != null;
+            }
+            if (nums[i] < t + Integer.MIN_VALUE) {
+                // 负数溢出(此时必然不会有正溢出)
+                return minTreeMap.lowerKey(maxBorder + 1) != null;
+            }
+            if (maxBorder > minTreeMap.firstKey()) {
+                Object temp = minTreeMap.higherKey(minBorder - 1);
+                if (temp != null) {
+                    int tempNum = (int) temp;
+                    if (tempNum <= maxBorder) {
+                        return true;
+                    }
+                }
+            } else if (maxBorder == minTreeMap.firstKey()) {
+                return true;
+            }
+            // 移除操作
+            int numToRemove = nums[i + 1];
+            int temp;
+            for (int j = 0; j < maps.size(); j++) {
+                TreeMap<Integer, Integer> map = maps.get(j);
+                temp = map.get(numToRemove);
+                if (temp == 1) {
+                    map.remove(numToRemove);
+                } else {
+                    map.put(numToRemove, temp - 1);
+                }
+            }
+            if (i + k + 1 < nums.length) {
+                // 添加操作
+                int numToAdd = nums[i + k + 1];
+                for (int j = 0; j < maps.size(); j++) {
+                    TreeMap<Integer, Integer> map = maps.get(j);
+                    map.put(numToAdd, map.getOrDefault(numToAdd, 0) + 1);
                 }
             }
         }
