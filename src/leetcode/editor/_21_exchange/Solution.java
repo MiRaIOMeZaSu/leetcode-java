02db03e9aaa989739f4a205dfb8b01cf548d4c30
miraiomezasu <3792927266@qq.com>
2021-06-18 23:28:43 +0800

    offer21:done

---
+++ src/leetcode/editor/_21_exchange/Solution.java
@@ -0,0 +1,21 @@
+package leetcode.editor._21_exchange;
+
+class Solution {
+    public int[] exchange(int[] nums) {
+        // 奇数从0开始放,偶数从-1开始放
+        int size = nums.length;
+        int left = 0;
+        int right = size - 1;
+        while (left < right) {
+            if (nums[left] % 2 == 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                right--;
+            } else {
+                left += 1;
+            }
+        }
+        return nums;
+    }
+}
