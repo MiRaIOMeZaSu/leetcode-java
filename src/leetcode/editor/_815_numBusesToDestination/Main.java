01a98cb476ec36da6d86c837590be163761f61a2
miraiomezasu <3792927266@qq.com>
2021-06-28 23:23:09 +0800

    815:TLE

---
+++ src/leetcode/editor/_815_numBusesToDestination/Main.java
@@ -0,0 +1,9 @@
+package leetcode.editor._815_numBusesToDestination;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] input = new int[][]{{1, 2, 7}, {3, 6, 7}};
+        solution.numBusesToDestination(input, 1, 6);
+    }
+}
--- src/leetcode/editor/_815_numBusesToDestination/Solution.java
+++ src/leetcode/editor/_815_numBusesToDestination/Solution.java
@@ -1,23 +1,23 @@
 package leetcode.editor._815_numBusesToDestination;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 class Solution {
-    HashMap<Integer, List<int[]>> map = new HashMap<>();
+    HashMap<Integer, List<Integer>> map = new HashMap<>();
     HashMap<Integer, Integer> dp = new HashMap<>();
     int targetPos;
+    int[][] graph;
 
     public int numBusesToDestination(int[][] routes, int source, int target) {
         // 实时更新相应站点的所有趟次
+        graph = routes;
         targetPos = target;
         for (int i = 0; i < routes.length; i++) {
             for (int j = 0; j < routes[i].length; j++) {
                 if (!map.containsKey(routes[i][j])) {
                     map.put(routes[i][j], new ArrayList<>());
                 }
-                map.get(routes[i][j]).add(routes[i]);
+                map.get(routes[i][j]).add(i);
             }
         }
         dp.put(source, 0);
@@ -32,11 +32,12 @@ class Solution {
         if (dp.containsKey(targetPos) && dp.get(targetPos) <= destinationNum) {
             return;
         }
-        List<int[]> arr = map.get(curr);
-        for (int i = 0; i < arr.size(); i++) {
-            for (int j = 0; j < arr.get(i).length; j++) {
-                int num = arr.get(i)[j];
-                if (!dp.containsKey(num) || destinationNum > dp.get(num)) {
+        List<Integer> list = map.get(curr);
+        for (int i = 0; i < list.size(); i++) {
+            int[] arr = graph[list.get(i)];
+            for (int j = 0; j < arr.length; j++) {
+                int num = arr[j];
+                if (!dp.containsKey(num) || destinationNum < dp.get(num)) {
                     dp.put(num, destinationNum);
                     solve(num, destinationNum + 1);
                 }
