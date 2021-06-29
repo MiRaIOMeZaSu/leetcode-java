e37b8df2a847e306a58a841e9e4cd96b32efb303
miraiomezasu <3792927266@qq.com>
2021-06-29 13:27:41 +0800

    909:done

--- src/leetcode/editor/_909_snakesAndLadders/Main.java
+++ src/leetcode/editor/_909_snakesAndLadders/Main.java
@@ -3,7 +3,14 @@ package leetcode.editor._909_snakesAndLadders;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[][] input = new int[][]{{-1, -1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1}, {-1, 35, -1, -1, 13, -1}, {-1, -1, -1, -1, -1, -1}, {-1, 15, -1, -1, -1, -1}};
+        int[][] input = new int[][]{
+                {-1, -1, 27, 13, -1, 25, -1},
+                {-1, -1, -1, -1, -1, -1, -1},
+                {44, -1, 8, -1, -1, 2, -1},
+                {-1, 30, -1, -1, -1, -1, -1},
+                {3, -1, 20, -1, 46, 6, -1},
+                {-1, -1, -1, -1, -1, -1, 29},
+                {-1, 29, 21, 33, -1, -1, -1}};
         solution.snakesAndLadders(input);
     }
 }
--- src/leetcode/editor/_909_snakesAndLadders/Solution.java
+++ src/leetcode/editor/_909_snakesAndLadders/Solution.java
@@ -1,44 +1,75 @@
 package leetcode.editor._909_snakesAndLadders;
 
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.LinkedList;
 
 class Solution {
     int N;
     HashMap<Integer, int[]> reflect = new HashMap<>();
-    int[][] dp;
+    Deque<Integer> q = new LinkedList<>();
+    int[][][] dp;
 
     public int snakesAndLadders(int[][] board) {
         // 回溯法
         // 动态规划
         // 优先寻找蛇和梯子
+        // 蛇和梯子的传送是强制的
+        // 蛇和梯子的使用应该表示出来
         N = board.length;
-        dp = new int[N][N];
+        dp = new int[N][N][2];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                dp[i][j] = Integer.MAX_VALUE;
+                for (int x = 0; x < 2; x++) {
+                    dp[i][j][x] = Integer.MAX_VALUE;
+                }
             }
         }
-        dp[N - 1][0] = 0;
+        dp[N - 1][0][0] = 0;
         int pivot = N * N;
         for (int i = 1; i <= pivot; i++) {
+            q.add(i);
+        }
+        while (!q.isEmpty()) {
+            int i = q.pollFirst();
             int[] pos = getTarget(i);
-            if (board[pos[0]][pos[1]] > i) {
+            if (board[pos[0]][pos[1]] != -1) {
+                // 强制跳转
                 int[] target = getTarget(board[pos[0]][pos[1]]);
-                dp[target[0]][target[1]] = Math.min(dp[pos[0]][pos[1]], dp[target[0]][target[1]]);
+                if (dp[pos[0]][pos[1]][0] < dp[target[0]][target[1]][1]) {
+                    dp[target[0]][target[1]][1] = dp[pos[0]][pos[1]][0];
+                    q.push(board[pos[0]][pos[1]]);
+                }
             }
-            for (int j = i + 1; j < i + 6 && j <= pivot; j++) {
-                solve(j, dp[pos[0]][pos[1]] + 1);
+            if (board[pos[0]][pos[1]] == -1 || dp[pos[0]][pos[1]][1] != Integer.MAX_VALUE) {
+                // 到此不需要强制跳转
+                int stop = 0;
+                if (board[pos[0]][pos[1]] != -1) {
+                    stop++;
+                }
+                for (int j = i + 1; j <= i + 6 && j <= pivot; j++) {
+                    solve(j, pos, stop);
+                }
             }
         }
-        return dp[0][0];
+
+        int[] endPos = getTarget(pivot);
+        int res = Math.min(dp[endPos[0]][endPos[1]][0], dp[endPos[0]][endPos[1]][1]);
+        return res != Integer.MAX_VALUE ? res : -1;
     }
 
-    private void solve(int target, int num) {
+    private void solve(int target, int[] sourcePos, int stop) {
         int[] pos = getTarget(target);
-        if (num >= dp[pos[0]][pos[1]]) {
-            return;
+        int temp = dp[pos[0]][pos[1]][0];
+        for (int i = 1; i >= stop; i--) {
+            if (dp[sourcePos[0]][sourcePos[1]][i] == Integer.MAX_VALUE) {
+                continue;
+            }
+            dp[pos[0]][pos[1]][0] = Math.min(dp[sourcePos[0]][sourcePos[1]][i] + 1, dp[pos[0]][pos[1]][0]);
         }
-        dp[pos[0]][pos[1]] = num;
+        if (dp[pos[0]][pos[1]][0] != temp) {
+            q.push(target);
+        }
     }
 
     private int[] getTarget(int index) {
