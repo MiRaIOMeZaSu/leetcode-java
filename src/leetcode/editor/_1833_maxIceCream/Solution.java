d228ec57f9703cb0a856df72a48383a0a8bd5db5
miraiomezasu <3792927266@qq.com>
2021-07-02 00:43:36 +0800

    1833:slow way

--- src/leetcode/editor/_1833_maxIceCream/Solution.java
+++ src/leetcode/editor/_1833_maxIceCream/Solution.java
@@ -1,38 +1,17 @@
 package leetcode.editor._1833_maxIceCream;
 
+import java.util.Arrays;
+
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int index = 0;
         int size = costs.length;
-        if (size == 1) {
-            if (coins >= costs[0]) {
-                return 1;
-            }
-            return 0;
-        }
         int sum = 0;
-        for (int i = 0; i < size; i++) {
-            sum += costs[i];
+        while (index < size && sum + costs[index] <= coins) {
+            sum += costs[index];
+            index++;
         }
-        if (sum <= coins) {
-            return size;
-        }
-        int res = 0;
-        while (coins > 0) {
-            int index = 0;
-            for (int i = 1; i < size - res; i++) {
-                if (costs[index] > costs[i]) {
-                    index = i;
-                }
-            }
-            if (coins < costs[index]) {
-                return res;
-            }
-            coins -= costs[index];
-            int temp = costs[index];
-            costs[index] = costs[size - res - 1];
-            costs[size - res - 1] = temp;
-            res += 1;
-        }
-        return res;
+        return index;
     }
 }
