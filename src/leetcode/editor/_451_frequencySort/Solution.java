bee38a73d2d39c8590ac3f0f295327769d325b12
miraiomezasu <3792927266@qq.com>
2021-07-03 09:05:00 +0800

    451:done

--- src/leetcode/editor/_451_frequencySort/Solution.java
+++ src/leetcode/editor/_451_frequencySort/Solution.java
@@ -5,24 +5,13 @@ import java.util.Comparator;
 
 class Solution {
     public String frequencySort(String s) {
-        char[] chars = new char[52];
-        chars[0] = 'A';
-        chars[26] = 'a';
-        for (int i = 0; i < 25; i++) {
-            chars[i + 1]  = (char) (chars[i] + 1);
-            chars[i + 27]  = (char) (chars[i + 26] + 1);
-        }
-        int[][] count = new int[52][2];
+        int[][] count = new int[95][2];
         for (int i = 0; i < s.length(); i++) {
-            int index = s.charAt(i) - 'A';
-            if (index > 25) {
-                index += 26 - 32;
-            }
+            int index = s.charAt(i) - ' ';
             count[index][0]++;
+            count[index][1] = index;
         }
-        for (int i = 0; i < 52; i++) {
-            count[i][1] = i;
-        }
+
         Arrays.sort(count, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
@@ -31,9 +20,9 @@ class Solution {
         });
         int index = 0;
         StringBuilder builder = new StringBuilder();
-        while (index < 52 && count[index][0] > 0) {
+        while (index < 95 && count[index][0] > 0) {
             for (int i = 0; i < count[index][0]; i++) {
-                builder.append(chars[count[index][1]]);
+                builder.append((char)(' ' + count[index][1]));
             }
             index++;
         }
