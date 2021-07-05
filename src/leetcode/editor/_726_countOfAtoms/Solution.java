76749949349be73b23e7fdceaa2c1a0194547406
miraiomezasu <3792927266@qq.com>
2021-07-05 10:44:00 +0800

    726:done

--- src/leetcode/editor/_726_countOfAtoms/Solution.java
+++ src/leetcode/editor/_726_countOfAtoms/Solution.java
@@ -1,55 +1,70 @@
 package leetcode.editor._726_countOfAtoms;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 class Solution {
     String string;
     int size;
-    int index = 0;
+    int index;
+    Map<String, Integer> map = new TreeMap<>();
 
     public String countOfAtoms(String formula) {
         // 线段法?
         // 栈
-        List<Integer> head = new ArrayList<>();
-        List<Integer> tail = new ArrayList<>();
-        List<Integer> nums = new ArrayList<>();
-        HashMap<String, Integer> map = new HashMap<>();
+        // 从后往前,用函数栈的方式
+        string = formula;
         size = formula.length();
-        int i = 0;
-        while (i < size) {
-            if (Character.isUpperCase(formula.charAt(i))) {
-                int start = i;
-                i++;
-                while (i < size && Character.isLowerCase(formula.charAt(i))) {
-                    i++;
-                }
-                map.put(formula.substring(start, i), 0);
-            } else if (i < size && Character.isDigit(formula.charAt(i))) {
-                i++;
-            } else if (formula.charAt(i) == '(') {
-                head.add(i);
-                i++;
-            } else if (formula.charAt(i) == ')') {
-                tail.add(i);
-                i++;
-                int num = 0;
-                while (i < size && Character.isDigit(formula.charAt(i))) {
-                    num *= 10;
-                    num += formula.charAt(i);
-                    i++;
-                }
-                if (num == 0) {
-                    num = 1;
-                }
-                nums.add(num);
+        index = size - 1;
+        solve(1);
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String key : map.keySet()) {
+            stringBuilder.append(key);
+            int count = map.get(key);
+            if (count != 1) {
+                stringBuilder.append(count);
             }
         }
-        // 从此开始线段划分
-        for (i = 0; i < nums.size(); i++) {
+        return stringBuilder.toString();
+    }
 
+    private void solve(int times) {
+        int pivot = 1;
+        StringBuilder stringBuilder = new StringBuilder();
+        while (index >= 0) {
+            if (Character.isDigit(string.charAt(index))) {
+                int num = 0;
+                int mutil = 1;
+                while (Character.isDigit(string.charAt(index))) {
+                    num += mutil * (string.charAt(index) - '0');
+                    mutil *= 10;
+                    index--;
+                }
+                pivot = num;
+                continue;
+            } else if (Character.isLowerCase(string.charAt(index))) {
+                stringBuilder.append(string.charAt(index));
+            } else if (Character.isUpperCase(string.charAt(index))) {
+                stringBuilder.append(string.charAt(index));
+                stringBuilder.reverse();
+                String key = stringBuilder.toString();
+                map.put(key, map.getOrDefault(key, 0) + pivot * times);
+                stringBuilder = new StringBuilder();
+                pivot = 1;
+            } else if (string.charAt(index) == ')') {
+                index--;
+                solve(times * pivot);
+                pivot = 1;
+                continue;
+            } else if (string.charAt(index) == '(') {
+                index--;
+                return;
+            }
+            index--;
         }
-        return "";
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.countOfAtoms("Mg(OH)2");
     }
 }
