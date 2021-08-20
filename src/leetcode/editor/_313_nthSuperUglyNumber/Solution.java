b43b8219ffd8a36cf384d9d6626ffe1690e5d607
miraiomezasu <3792927266@qq.com>
2021-08-20 08:54:19 +0800

    313:TLE

--- src/leetcode/editor/_313_nthSuperUglyNumber/Solution.java
+++ src/leetcode/editor/_313_nthSuperUglyNumber/Solution.java
@@ -1,32 +1,27 @@
 package leetcode.editor._313_nthSuperUglyNumber;
 
+import java.util.*;
+
 class Solution {
     public int nthSuperUglyNumber(int n, int[] primes) {
-        // 使用指针法(10^8)
         // 最小堆
         // 动态规划?
         int size = primes.length;
-        int[] indexs = new int[size];
         int[] result = new int[n + 1];
-        for (int i = 0; i < size; i++) {
-            indexs[i] = 1;
-        }
         result[1] = 1;
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int i = 0; i < size; i++) {
+            set.add(primes[i]);
+        }
         for (int i = 2; i <= n; i++) {
-            int primeIndex = 0;
-            int pivot = result[indexs[0]] * primes[0];
-            for (int j = 1; j < size; j++) {
-                int temp = result[indexs[j]] * primes[j];
-                if (temp < pivot) {
-                    primeIndex = j;
-                    pivot = temp;
+            int temp = set.pollFirst();
+            result[i] = temp;
+            for (int j = 0; j < size; j++) {
+                int next = temp * primes[j];
+                if (Integer.MAX_VALUE / temp < primes[j]) {
+                    continue;
                 }
-            }
-            indexs[primeIndex]++;
-            if (pivot != result[i - 1]) {
-                result[i] = pivot;
-            } else {
-                i--;
+                set.add(next);
             }
         }
         return result[n];
