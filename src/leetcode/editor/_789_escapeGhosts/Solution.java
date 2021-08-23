1d821dd09b00b2da802f01b7c346e0e68e6acfe9
miraiomezasu <3792927266@qq.com>
2021-08-23 15:13:14 +0800

    789:wrong answer

--- src/leetcode/editor/_789_escapeGhosts/Solution.java
+++ src/leetcode/editor/_789_escapeGhosts/Solution.java
@@ -15,12 +15,20 @@ class Solution {
         this.ghosts = ghosts;
         m = target[1];
         n = target[0];
+        int toPi = 0;
+        int toPj = 0;
         for (int i = 0; i < ghosts.length; i++) {
+            toPi = Math.min(toPi, ghosts[i][0]);
+            toPj = Math.min(toPj, ghosts[i][1]);
             m = Math.max(m + 1, ghosts[i][0] + 1);
             n = Math.max(n + 1, ghosts[i][1] + 1);
         }
-        graph = new int[m][n];
-        visit = new int[m][n];
+        for (int i = 0; i < ghosts.length; i++) {
+            ghosts[i][0] -= toPi;
+            ghosts[i][1] -= toPj;
+        }
+        graph = new int[m - toPi][n - toPj];
+        visit = new int[m - toPi][n - toPj];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 graph[i][j] = Integer.MAX_VALUE;
@@ -30,7 +38,9 @@ class Solution {
         for (int i = 0; i < ghosts.length; i++) {
             draw(ghosts[i][0], ghosts[i][1], 0);
         }
-        return solve(0, 0, 0);
+        target[0] -=toPi;
+        target[1] -=toPj;
+        return solve(-toPi, -toPj, 0);
     }
 
     private void draw(int i, int j, int val) {
@@ -51,7 +61,7 @@ class Solution {
         if (!(i >= 0 && j >= 0 && i < m && j < n)) {
             return false;
         }
-        if (val > graph[i][j]) {
+        if (val >= graph[i][j]) {
             return false;
         }
         if (visit[i][j] <= val) {
@@ -61,20 +71,20 @@ class Solution {
         if (i == target[0] && j == target[1]) {
             return true;
         }
-        int a1 = i + 1;
-        int b1 = j;
-        int a2 = i;
-        int b2 = j + 1;
-        if (Math.pow(a1 - target[0], 2) + Math.pow(b1 - target[1], 2) <=
-                Math.pow(a2 - target[0], 2) + Math.pow(b2 - target[1], 2)) {
-            return solve(a1, b1, val + 1) || solve(a2, b2, val + 1);
-        } else {
-            return solve(a2, b2, val + 1) || solve(a1, b1, val + 1);
+        for (int s = 0; s < 4; s++) {
+            int y = i + items[s][0];
+            int x = j + items[s][1];
+            if (y >= 0 && x >= 0 && y < m && x < n) {
+                if (solve(y, x, val + 1)) {
+                    return true;
+                }
+            }
         }
+        return false;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.escapeGhosts(new int[][]{{1, 0}, {0, 3}}, new int[]{0, 1}));
+        System.out.println(solution.escapeGhosts(new int[][]{{5,0},{-10,-2},{0,-5},{-2,-2},{-7,1}}, new int[]{7,7}));
     }
 }
