80f0ad64bc36df76016e9be62e75b16063f8479f
miraiomezasu <3792927266@qq.com>
2021-09-05 11:51:09 +0800

    5864:done

--- src/leetcode/editor/_5864_numberOfWeakCharacters/Solution.java
+++ src/leetcode/editor/_5864_numberOfWeakCharacters/Solution.java
@@ -1,7 +1,6 @@
 package leetcode.editor._5864_numberOfWeakCharacters;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 class Solution {
     public int numberOfWeakCharacters(int[][] properties) {
@@ -12,16 +11,36 @@ class Solution {
             }
             return o1[1] - o2[1];
         });
-        for (int i = 1; i < size; i++) {
-            if (properties[i][0] <= properties[0][0] || properties[i][1] <= properties[0][1]) {
-                return 0;
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int i = 0; i < size; i++) {
+            map.merge(properties[i][1], 1, Integer::sum);
+        }
+        int last = -1;
+        int result = 0;
+        for (int i = 0; i < size; i++) {
+            if (last != properties[i][0]) {
+                for (int j = i; j < size && properties[j][0] == properties[i][0]; j++) {
+                    map.merge(properties[j][1], -1, Integer::sum);
+                }
+                last = properties[i][0];
+            }
+            while (!map.isEmpty() && map.lastEntry().getValue() <= 0) {
+                map.pollLastEntry();
+            }
+            if (map.isEmpty()) {
+                break;
+            }
+            Integer temp = map.lastKey();
+            if (temp != null && temp > properties[i][1]) {
+                result += 1;
             }
         }
-        return 1;
+        return result;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.numberOfWeakCharacters(new int[][]{{1, 5}, {10, 4}, {4, 3}});
+        int ret = solution.numberOfWeakCharacters(new int[][]{{1, 5}, {10, 4}, {4, 3}});
+        System.out.println(ret);
     }
 }
