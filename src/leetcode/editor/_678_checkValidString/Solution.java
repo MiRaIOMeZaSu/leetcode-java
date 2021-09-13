7c85e81d87294f50692177cf1caa1a85900250c0
miraiomezasu <379297266@qq.com>
2021-09-13 14:57:57 +0800

    678:still wrong understand

--- src/leetcode/editor/_678_checkValidString/Solution.java
+++ src/leetcode/editor/_678_checkValidString/Solution.java
@@ -9,8 +9,6 @@ class Solution {
     char leftBracket = '(';
     char rightBracket = ')';
     char blank = '*';
-    int pivot;
-    int blankCount = 0;
 
     public boolean checkValidString(String s) {
         // 计算左右括号的数量差
@@ -22,57 +20,41 @@ class Solution {
             if (chars[i] == leftBracket) {
                 count[0]++;
             } else if (chars[i] == blank) {
-                blankCount++;
-            } else {
+            } else if (chars[i] == rightBracket) {
                 count[1]++;
             }
         }
-        pivot = Math.abs(count[0] - count[1]);
-//        if (pivot > blankCount) {
-//            return false;
-//        }
-
-        if (solve(chars, new LinkedList<>(), 0, 0)) {
-            return true;
-        }
-        char[] chars1 = new char[size];
-        HashMap<Character,Character> hashMap = new HashMap<>();
-        hashMap.put('(',')');
-        hashMap.put(')','(');
-        hashMap.put('*','*');
-        for (int i = 0; i < size; i++) {
-            chars1[i] = hashMap.get(chars[size - 1 - i]);
-        }
-        return solve(chars1, new LinkedList<>(), 0, 0);
+        return solve(chars, new LinkedList<>(), 0);
     }
 
-    private boolean solve(char[] chars, Deque<Character> stack, int index, int blankUsedCount) {
+    private boolean solve(char[] chars, Deque<Character> stack, int index) {
         if (index >= chars.length && stack.isEmpty()) {
             return true;
         }
-//        if (blankUsedCount > pivot) {
-//            return false;
-//        }
         for (int i = index; i < chars.length; i++) {
-            if (chars[i] != rightBracket) {
+            if (chars[i] == leftBracket) {
                 stack.push(chars[i]);
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                if (stack.peek() == leftBracket) {
+            } else if (chars[i] == rightBracket) {
+                if (!stack.isEmpty()) {
                     stack.pop();
                 } else {
-                    // 下一轮递归
-                    stack.pop();
-                    boolean a = solve(chars, new LinkedList<>(stack), i + 1, blankUsedCount + 1);
-                    if (a) {
-                        return true;
-                    } else {
-                        return solve(chars, stack, i, blankUsedCount);
-                    }
+                    return false;
                 }
-
+            } else if (chars[i] == blank) {
+                if (solve(chars, new LinkedList<>(stack), i + 1)) {
+                    return true;
+                }
+                Deque<Character> temp = new LinkedList<>(stack);
+                temp.push(leftBracket);
+                if (solve(chars, new LinkedList<>(temp), i + 1)) {
+                    return true;
+                }
+                if (!stack.isEmpty()) {
+                    temp = new LinkedList<>(stack);
+                    temp.pop();
+                    return solve(chars, new LinkedList<>(temp), i + 1);
+                }
+                return false;
             }
         }
         // 此时遍历到末尾
@@ -87,8 +69,6 @@ class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.checkValidString("**))*(*"));
-//        System.out.println(solution.checkValidString("(*)"));
-//        System.out.println(solution.checkValidString("(*)))"));
+        System.out.println(solution.checkValidString("*)"));
     }
 }
