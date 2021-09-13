ee58140fafae6a4c0bb1ef6965fcc5eaad112e2f
miraiomezasu <379297266@qq.com>
2021-09-13 14:21:11 +0800

    678:wrong understand

--- src/leetcode/editor/_678_checkValidString/Solution.java
+++ src/leetcode/editor/_678_checkValidString/Solution.java
@@ -1,30 +1,94 @@
 package leetcode.editor._678_checkValidString;
 
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 
 class Solution {
+    char leftBracket = '(';
+    char rightBracket = ')';
+    char blank = '*';
+    int pivot;
+    int blankCount = 0;
+
     public boolean checkValidString(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        map.put('(', 0);
-        map.put(')', 1);
-        map.put('*', 2);
+        // 计算左右括号的数量差
+        // 使用回溯法,选择当做空的或选择当做括号
+        int[] count = new int[2];
         int size = s.length();
-        int[][] fore = new int[s.length()][3];
-        int[][] tail = new int[s.length()][3];
-        fore[0][map.get(s.charAt(0))] += 1;
-        for (int i = 1; i < s.length(); i++) {
-            for (int j = 0; j < 3; j++) {
-                fore[i][j] = fore[i - 1][j];
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < size; i++) {
+            if (chars[i] == leftBracket) {
+                count[0]++;
+            } else if (chars[i] == blank) {
+                blankCount++;
+            } else {
+                count[1]++;
             }
-            fore[i][map.get(s.charAt(i))]++;
         }
-        for (int i = size - 2; i >= 0; i--) {
-            for (int j = 0; j < 3; j++) {
-                fore[i][j] = fore[i - 1][j];
+        pivot = Math.abs(count[0] - count[1]);
+//        if (pivot > blankCount) {
+//            return false;
+//        }
+
+        if (solve(chars, new LinkedList<>(), 0, 0)) {
+            return true;
+        }
+        char[] chars1 = new char[size];
+        HashMap<Character,Character> hashMap = new HashMap<>();
+        hashMap.put('(',')');
+        hashMap.put(')','(');
+        hashMap.put('*','*');
+        for (int i = 0; i < size; i++) {
+            chars1[i] = hashMap.get(chars[size - 1 - i]);
+        }
+        return solve(chars1, new LinkedList<>(), 0, 0);
+    }
+
+    private boolean solve(char[] chars, Deque<Character> stack, int index, int blankUsedCount) {
+        if (index >= chars.length && stack.isEmpty()) {
+            return true;
+        }
+//        if (blankUsedCount > pivot) {
+//            return false;
+//        }
+        for (int i = index; i < chars.length; i++) {
+            if (chars[i] != rightBracket) {
+                stack.push(chars[i]);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                if (stack.peek() == leftBracket) {
+                    stack.pop();
+                } else {
+                    // 下一轮递归
+                    stack.pop();
+                    boolean a = solve(chars, new LinkedList<>(stack), i + 1, blankUsedCount + 1);
+                    if (a) {
+                        return true;
+                    } else {
+                        return solve(chars, stack, i, blankUsedCount);
+                    }
+                }
+
             }
-            fore[i][map.get(s.charAt(i))]++;
         }
-        return false;
+        // 此时遍历到末尾
+        for (char ch : stack
+        ) {
+            if (ch != blank) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.checkValidString("**))*(*"));
+//        System.out.println(solution.checkValidString("(*)"));
+//        System.out.println(solution.checkValidString("(*)))"));
     }
 }
