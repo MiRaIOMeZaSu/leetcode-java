5be950303a13f18e070509f4cacc5417b9c9ada3
miraiomezasu <379297266@qq.com>
2021-09-17 10:47:19 +0800

    1043:slow way

--- src/leetcode/editor/_1043_maxSumAfterPartitioning/Solution.java
+++ src/leetcode/editor/_1043_maxSumAfterPartitioning/Solution.java
@@ -5,40 +5,36 @@ class Solution {
         // 简单的动态规划
         // 长度为500!
         int size = arr.length;
-        int[][][] dp = new int[size][size][k + 1];
+        // 优化方法: 使用同样是动态规划求出区间内最大值,避免核心代码每次都计算相同的部分?
+        int[][] dp = new int[size][size];
         for (int i = 0; i < size; i++) {
-            dp[i][i][1] = arr[i];
-            dp[i][i][1] = arr[i];
+            dp[i][i] = arr[i];
+            dp[i][i] = arr[i];
         }
         // 长度len
-        for (int len = 2; len <= size; len++) {
+        for (int len = 2; len <= k; len++) {
             // 起点i
             for (int i = 0; i + len - 1 < size; i++) {
                 int end = i + len - 1;
                 for (int j = i; j < i + len - 1; j++) {
-                    int maxNum = Math.max(dp[i][j][1]/(j - i + 1), dp[j + 1][end][1]/(end - j));
-                    dp[i][end][1] = Math.max(maxNum * len, dp[i][end][1]);
+                    int maxNum = Math.max(dp[i][j] / (j - i + 1), dp[j + 1][end] / (end - j));
+                    dp[i][end] = Math.max(maxNum * len, dp[i][end]);
                 }
             }
         }
-        for (int currK = 2; currK <= k; currK++) {
-            for (int i = currK; i <= size; i++) {
-                for (int m = 0; m + i - 1 < size; m++) {
-                    int start = m;
-                    int end = m + i - 1;
-                    for (int n = start; n < end; n++) {
-                        int temp = dp[start][n][1] + dp[n + 1][end][currK - 1];
-                        if (temp > dp[start][end][currK]) {
-                            if(start==0&&end==size - 1&&currK==k){
-                                System.out.println();
-                            }
-                            dp[start][end][currK] = temp;
-                        }
+        for (int i = 2; i <= size; i++) {
+            for (int m = 0; m + i - 1 < size; m++) {
+                int start = m;
+                int end = m + i - 1;
+                for (int n = start; n - start + 1 <= k && n < end; n++) {
+                    int temp = dp[start][n] + dp[n + 1][end];
+                    if (temp > dp[start][end]) {
+                        dp[start][end] = temp;
                     }
                 }
             }
         }
-        return dp[0][size - 1][k];
+        return dp[0][size - 1];
     }
 
     public static void main(String[] args) {
