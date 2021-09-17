79b84e95c336e68ea6855044cca6b14843125f9c
miraiomezasu <379297266@qq.com>
2021-09-17 11:04:32 +0800

    1043:still slow

--- src/leetcode/editor/_1043_maxSumAfterPartitioning/Solution.java
+++ src/leetcode/editor/_1043_maxSumAfterPartitioning/Solution.java
@@ -7,22 +7,19 @@ class Solution {
         int size = arr.length;
         // 优化方法: 使用同样是动态规划求出区间内最大值,避免核心代码每次都计算相同的部分?
         int[][] dp = new int[size][size];
-        for (int i = 0; i < size; i++) {
-            dp[i][i] = arr[i];
-            dp[i][i] = arr[i];
-        }
         // 长度len
-        for (int len = 2; len <= k; len++) {
+        for (int len = 1; len <= k; len++) {
             // 起点i
             for (int i = 0; i + len - 1 < size; i++) {
                 int end = i + len - 1;
-                for (int j = i; j < i + len - 1; j++) {
-                    int maxNum = Math.max(dp[i][j] / (j - i + 1), dp[j + 1][end] / (end - j));
-                    dp[i][end] = Math.max(maxNum * len, dp[i][end]);
+                int maxNum = arr[i];
+                for (int j = i + 1; j <= i + len - 1; j++) {
+                    maxNum = Math.max(maxNum,arr[j]);
                 }
+                dp[i][end] = Math.max(maxNum * len, dp[i][end]);
             }
         }
-        for (int i = 2; i <= size; i++) {
+        for (int i = k; i <= size; i++) {
             for (int m = 0; m + i - 1 < size; m++) {
                 int start = m;
                 int end = m + i - 1;
