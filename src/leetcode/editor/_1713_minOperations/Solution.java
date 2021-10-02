1d0d3b51d917c4f9c9b80e2a3300925d644c8c34
miraiomezasu <379297266@qq.com>
2021-10-02 20:34:11 +0800

    1713:still TLE

--- src/leetcode/editor/_1713_minOperations/Solution.java
+++ src/leetcode/editor/_1713_minOperations/Solution.java
@@ -13,25 +13,27 @@ class Solution {
         // 重点来了,target中的数字互不相同!
         int arrSize = arr.length;
         int targetSize = target.length;
-        Map<Integer, int[]> map = new HashMap<>(targetSize);
-        map.put(0, new int[]{0, 0});
-        int last = 0;
+        Map<Integer, Integer> map = new HashMap<>(targetSize);
         for (int i = 0; i < targetSize; i++) {
-            map.put(target[i], new int[]{last, 0});
-            last = target[i];
+            map.put(target[i], i);
         }
+        int[] dp = new int[targetSize];
         for (int i = 0; i < arrSize; i++) {
             if (map.containsKey(arr[i])) {
-                int[] pres = map.get(arr[i]);
-                int next = map.get(pres[0])[1] + 1;
-                if (next > pres[1]) {
-                    pres[1] = next;
+                int index = map.get(arr[i]);
+                int j = index - 1;
+                if (index == 0) {
+                    dp[index] = 1;
+                }
+                while (dp[index] < j + 2 && j >= 0) {
+                    dp[index] = Math.max(dp[index], dp[j] + 1);
+                    j--;
                 }
             }
         }
         int ans = 0;
         for (int i = 0; i < targetSize; i++) {
-            ans = Math.max(ans, map.get(target[i])[1]);
+            ans = Math.max(ans, dp[i]);
         }
         return targetSize - ans;
     }
@@ -39,9 +41,9 @@ class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.minOperations(new int[]{
-                5,1,3
+                11, 16, 20, 1, 2, 13, 7, 6, 12, 3
         }, new int[]{
-                9,4,2,3,4
+                11, 13, 3, 7, 7, 1, 10, 12, 14, 1
         }));
     }
 }
