f0e98cd342c0619b981ee37f2772e5d371a05a13
miraiomezasu <379297266@qq.com>
2021-10-02 20:12:59 +0800

    1713:still wrong

--- src/leetcode/editor/_1713_minOperations/Solution.java
+++ src/leetcode/editor/_1713_minOperations/Solution.java
@@ -10,69 +10,38 @@ class Solution {
         // 一遍寻找?
         // 寻找最长公共子序列
         // 直接寻找最长公共子序列
-        int longSize, shortSize;
-        int[] longWord, shortWord;
-        int len1 = target.length;
-        int len2 = arr.length;
-        boolean flag = len1 > len2;
-        longSize = flag ? len1 : len2;
-        shortSize = flag ? len2 : len1;
-        longWord = flag ? target : arr;
-        shortWord = flag ? arr : target;
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < longSize; i++) {
-            int index = longWord[i];
-            if (!map.containsKey(index)) {
-                map.put(index, i);
-                continue;
-            }
-            map.put(index, Math.min(i, map.get(index)));
+        // 重点来了,target中的数字互不相同!
+        int arrSize = arr.length;
+        int targetSize = target.length;
+        Map<Integer, int[]> map = new HashMap<>(targetSize);
+        map.put(0, new int[]{0, 0});
+        int last = 0;
+        for (int i = 0; i < targetSize; i++) {
+            map.put(target[i], new int[]{last, 0});
+            last = target[i];
         }
-        int[][] lastDp = new int[2][longSize];
-        Set<Integer> curr = new HashSet<>();
-        int index = shortWord[0];
-        curr.add(index);
-        if (map.containsKey(index)) {
-            for (int i = map.get(index); i < longSize; i++) {
-                lastDp[0][i] = 1;
-            }
-        }
-        for (int i = 1; i < shortSize; i++) {
-            int longIndex = longWord[0];
-            int shortIndex = shortWord[i];
-            curr.add(shortIndex);
-            if (curr.contains(longIndex)) {
-                lastDp[1][0] = 1;
-            }
-            for (int j = 1; j < longSize; j++) {
-                lastDp[1][j] = Math.max(lastDp[0][j], lastDp[1][j]);
-                lastDp[1][j] = Math.max(lastDp[1][j - 1], lastDp[1][j]);
-                if (longWord[j] == shortWord[i]) {
-                    lastDp[1][j] = Math.max(lastDp[0][j - 1] + 1, lastDp[1][j]);
+        for (int i = 0; i < arrSize; i++) {
+            if (map.containsKey(arr[i])) {
+                int[] pres = map.get(arr[i]);
+                int next = map.get(pres[0])[1] + 1;
+                if (next > pres[1]) {
+                    pres[1] = next;
                 }
             }
-            lastDp[0] = lastDp[1];
-            lastDp[1] = new int[longSize];
         }
-        return target.length - lastDp[0][longSize - 1];
-    }
-
-    private int max(int[] nums) {
-        int ans = Integer.MIN_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > ans) {
-                ans = nums[i];
-            }
+        int ans = 0;
+        for (int i = 0; i < targetSize; i++) {
+            ans = Math.max(ans, map.get(target[i])[1]);
         }
-        return ans;
+        return targetSize - ans;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.minOperations(new int[]{
-                6,4,8,1,3,2
+                5,1,3
         }, new int[]{
-                4,7,6,2,3,8,6,1
+                9,4,2,3,4
         }));
     }
 }
