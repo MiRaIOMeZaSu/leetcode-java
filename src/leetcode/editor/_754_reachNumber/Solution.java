a22326bbb2735dd2d3a102ea41965c132a288b57
miraiomezasu <379297266@qq.com>
2021-10-05 12:01:17 +0800

    754:done

--- src/leetcode/editor/_754_reachNumber/Solution.java
+++ src/leetcode/editor/_754_reachNumber/Solution.java
@@ -1,38 +1,24 @@
 package leetcode.editor._754_reachNumber;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-
 class Solution {
     public int reachNumber(int target) {
         // 使用(哈希链表)背包法
-        int level = (int) Math.sqrt(target);
-        ArrayList<Integer> list = new ArrayList<>(level);
-        HashSet<Integer> set = new HashSet<>(level);
-        list.add(0);
-        set.add(0);
-        int index = 1;
-        int currSum = 0;
+        target = Math.abs(target);
+        int index = (int) Math.sqrt(target * 2);
         while (true) {
-            currSum += index;
-            int currSize = list.size();
-            for (int i = 0; i < currSize; i++) {
-                int num = list.get(i);
-                if (2 * num - currSum == target) {
-                    return index;
-                }
-                int next = num + index;
-                if (!set.contains(next)) {
-                    if (2 * next - currSum == target) {
-                        return index;
-                    }
-                    set.add(next);
-                    list.add(next);
-                }
+            int sum = (index + 1) * index / 2;
+            int left = 2;
+            int right = sum * 2;
+            int twoNum = sum + target;
+            if (twoNum % 2 == 0 && twoNum >= left && twoNum <= right) {
+                return index;
             }
             index++;
         }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.reachNumber(2);
     }
 }
