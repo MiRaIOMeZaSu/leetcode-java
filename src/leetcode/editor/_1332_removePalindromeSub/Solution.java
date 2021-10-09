e8d893feb8e215dead3aff2a187e9a7f3afcdad4
miraiomezasu <379297266@qq.com>
2021-10-09 16:26:10 +0800

    1332: still understand

--- src/leetcode/editor/_1332_removePalindromeSub/Solution.java
+++ src/leetcode/editor/_1332_removePalindromeSub/Solution.java
@@ -4,31 +4,23 @@ class Solution {
     public int removePalindromeSub(String s) {
         // 每次直接寻找最长回文子序列
         // 最长为1000
+        int[] bits = new int[]{1, 2};
         int size = s.length();
-        if (size == 0) {
-            return 0;
-        }
         char[] chars = s.toCharArray();
-        int left = 0;
-        int right = size - 1;
-        boolean[] toRemove = new boolean[size];
-        while (left <= right) {
-            if (chars[left] == chars[right]) {
-                toRemove[left] = true;
-                toRemove[right] = true;
-                left++;
-                right--;
+        int currBit = 0;
+        for (int i = 0; i < size && currBit != 3; i++) {
+            if (chars[i] == 'a') {
+                currBit |= bits[0];
             } else {
-                right--;
+                currBit |= bits[1];
             }
         }
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < size; i++) {
-            if (!toRemove[i]) {
-                stringBuilder.append(chars[i]);
-            }
+        if (currBit == 3) {
+            // 判断本身是否为回文
+
+            return 2;
         }
-        return 1 + removePalindromeSub(stringBuilder.toString());
+        return 1;
     }
 
     public static void main(String[] args) {
