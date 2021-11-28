274c782e9e2fa20bff4cff6bdb6b421cb0cef38f
miraiomezasu <379297266@qq.com>
2021-11-28 11:40:18 +0800

    5941: still wrong

--- src/leetcode/editor/contest/_5941_findAllPeople/Solution.java
+++ src/leetcode/editor/contest/_5941_findAllPeople/Solution.java
@@ -22,7 +22,11 @@ class Solution {
             if (graph[meetings[i][0]] == null) {
                 graph[meetings[i][0]] = new ArrayList<>();
             }
+            if (graph[meetings[i][1]] == null) {
+                graph[meetings[i][1]] = new ArrayList<>();
+            }
             graph[meetings[i][0]].add(meetings[i]);
+            graph[meetings[i][1]].add(meetings[i]);
         }
         // 开始循环
         solve(0);
@@ -37,10 +41,37 @@ class Solution {
     private void solve(int startPerson) {
         for (int i = 0; graph[startPerson] != null && i < graph[startPerson].size(); i++) {
             int[] meeting = graph[startPerson].get(i);
-            if (meeting[2] + 1 >= time[startPerson]) {
-                if (time[meeting[1]] == 0 || time[meeting[1]] > meeting[2] + 1) {
+            int min = Math.min(time[meeting[0]], time[meeting[1]]);
+            int max = Math.max(time[meeting[0]], time[meeting[1]]);
+            if (min == 0) {
+                min = max;
+            }
+            if (meeting[2] + 1 < min) {
+                // 开会时间太早,没用
+                continue;
+            }
+            if (time[meeting[0]] == time[meeting[1]]) {
+                continue;
+            }
+            if (time[meeting[1]] == 0) {
+                time[meeting[1]] = meeting[2] + 1;
+                solve(meeting[1]);
+                continue;
+            } else if (time[meeting[0]] == 0) {
+                time[meeting[0]] = meeting[2] + 1;
+                solve(meeting[0]);
+                continue;
+            }
+            // 此时开始寻找谁给谁同步的情况(两者都有初始时间)
+            if (meeting[2] + 1 < max) {
+                // 可以起到更新的效果
+                if (time[meeting[1]] > meeting[2] + 1) {
                     time[meeting[1]] = meeting[2] + 1;
                     solve(meeting[1]);
+                } else {
+                    time[meeting[0]] = meeting[2] + 1;
+                    solve(meeting[0]);
+                    return;
                 }
             }
         }
@@ -48,6 +79,6 @@ class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.findAllPeople(6, new int[][]{{0, 2, 1}, {1, 3, 1}, {4, 5, 1}}, 1));
+        System.out.println(solution.findAllPeople(4, new int[][]{{3, 1, 3}, {1, 2, 2}, {0, 3, 3}}, 3));
     }
 }
