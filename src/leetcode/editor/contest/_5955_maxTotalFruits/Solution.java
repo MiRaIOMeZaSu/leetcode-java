56cbcd6ac5567e6d4b14a5fd3fecb331337514f7
miraiomezasu <379297266@qq.com>
2021-12-12 12:28:14 +0800

    5955: done

--- src/leetcode/editor/contest/_5955_maxTotalFruits/Solution.java
+++ src/leetcode/editor/contest/_5955_maxTotalFruits/Solution.java
@@ -9,6 +9,10 @@ class Solution {
         // 使用前缀法
         int size = fruits.length;
         int positionBorder = fruits[size - 1][0];
+        if (startPos > positionBorder) {
+            k -= startPos - positionBorder;
+            startPos = positionBorder;
+        }
         int[] position = new int[positionBorder + 2];
         int last = 0;
         int currIndex = 0;
@@ -28,7 +32,7 @@ class Solution {
                 right = currIndex;
             }
             if (fruit[0] == startPos) {
-                thisPosition += fruit[1];
+                thisPosition = fruit[1];
             }
             // 开始进行position的插入?
             if (fruit[0] == i) {
@@ -36,23 +40,31 @@ class Solution {
             }
             position[i + 1] = last;
         }
+        if (k == 0) {
+            return thisPosition;
+        } else if (k < 0) {
+            return 0;
+        }
         // 此时前缀表构建完毕
         int minLeft = Math.max(0, startPos - k);
         ans = Math.max(ans, position[startPos + 1] - position[minLeft]);
         // 往前走
+        if (right == -1) {
+            return ans;
+        }
         for (int i = right; i < size; i++) {
             // 先往右走
             int needle = fruits[i][0] - startPos;
             int rest = k - needle;
             if (rest >= 0) {
-                int currLeft = Math.min(startPos, fruits[i][0] - rest);
+                int currLeft = Math.min(startPos, Math.max(fruits[i][0] - rest, 0));
                 ans = Math.max(ans, position[fruits[i][0] + 1] - position[currLeft]);
             } else {
                 // 说明无法到达此处
                 break;
             }
             // 先往左走
-            int currLeft = (startPos - rest/2);
+            int currLeft = Math.max((startPos - rest / 2), 0);
             ans = Math.max(ans, position[fruits[i][0] + 1] - position[currLeft]);
         }
         return ans;
@@ -60,6 +72,7 @@ class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.maxTotalFruits(new int[][]{{2, 8}, {6, 3}, {8, 6}}, 5, 4);
+        System.out.println(solution.maxTotalFruits(new int[][]{{5, 8}, {7, 7}, {8, 7}, {15, 5}, {18, 8}, {19, 3}, {25, 4}, {26, 1}, {34, 10}, {38, 3}, {39, 4}, {40, 5}}, 6,
+                19));
     }
 }
