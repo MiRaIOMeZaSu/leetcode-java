dd97c84f2e891f972cd933902fc7d0c7a7e5c88c
miraiomezasu <379297266@qq.com>
2022-04-16 16:17:43 +0800

    lccup3: done

---
+++ src/leetcode/editor/contest/_6019_replaceNonCoprimes/Solution.java
@@ -0,0 +1,15 @@
+package leetcode.editor.contest._6019_replaceNonCoprimes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> replaceNonCoprimes(int[] nums) {
+        // 如何确认有公约数
+        // 如何求取最小公倍数
+        // 使用链表
+        List<Integer> ans = new ArrayList<>();
+
+        return ans;
+    }
+}
